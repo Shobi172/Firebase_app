bccc809dcaeea0ce4087c57beac1ad905790df81
Shobin Shaju <shobinshaju2001@gmail.com>
2023-09-26 14:45:38 +0530

    firebase config modified

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDyjz7v520ChZfA9ldCiDZg1KR3seTYEFI",
+  authDomain: "user-management-app-6a49e.firebaseapp.com",
+  databaseURL: "https://user-management-app-6a49e-default-rtdb.firebaseio.com",
+  projectId: "user-management-app-6a49e",
+  storageBucket: "user-management-app-6a49e.appspot.com",
+  messagingSenderId: "1017475657723",
+  appId: "1:1017475657723:web:0e3dbbe485acd0fb51e55e"
 };
 
 const app = initializeApp(firebaseConfig);
